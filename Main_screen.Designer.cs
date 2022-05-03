2857f4bc2ce61ecce35d1762d1965d9e6a87dc4d
Nicolai <99505820+KeyK4@users.noreply.github.com>
2022-05-03 20:30:32 +0400

    Design2
    Добавлен файл данных, поля с процентом по категориям, кнопка "цель". Допилено поле с историей операций. Убраны графики.

--- Main_screen.Designer.cs
+++ Main_screen.Designer.cs
@@ -28,18 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.cost = new System.Windows.Forms.Button();
             this.income = new System.Windows.Forms.Button();
             this.action_textbox = new System.Windows.Forms.TextBox();
-            this.cost_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.cost_chart)).BeginInit();
+            this.cost_category = new System.Windows.Forms.TextBox();
+            this.income_category = new System.Windows.Forms.TextBox();
+            this.goal = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // cost
             // 
-            this.cost.Location = new System.Drawing.Point(145, 166);
+            this.cost.Location = new System.Drawing.Point(91, 22);
             this.cost.Name = "cost";
             this.cost.Size = new System.Drawing.Size(124, 40);
             this.cost.TabIndex = 0;
@@ -49,17 +48,18 @@
             // 
             // income
             // 
-            this.income.Location = new System.Drawing.Point(409, 166);
+            this.income.Location = new System.Drawing.Point(273, 22);
             this.income.Name = "income";
             this.income.Size = new System.Drawing.Size(124, 40);
             this.income.TabIndex = 1;
             this.income.Text = "Доход";
             this.income.UseVisualStyleBackColor = true;
+            this.income.Click += new System.EventHandler(this.income_Click);
             // 
             // action_textbox
             // 
             this.action_textbox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.action_textbox.Location = new System.Drawing.Point(63, 212);
+            this.action_textbox.Location = new System.Drawing.Point(68, 81);
             this.action_textbox.Multiline = true;
             this.action_textbox.Name = "action_textbox";
             this.action_textbox.ReadOnly = true;
@@ -67,33 +67,47 @@
             this.action_textbox.TabIndex = 2;
             this.action_textbox.TextChanged += new System.EventHandler(this.action_textbox_TextChanged);
             // 
-            // cost_chart
+            // cost_category
             // 
-            chartArea1.Name = "ChartArea1";
-            this.cost_chart.ChartAreas.Add(chartArea1);
-            this.cost_chart.Location = new System.Drawing.Point(63, 16);
-            this.cost_chart.Name = "cost_chart";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.cost_chart.Series.Add(series1);
-            this.cost_chart.Size = new System.Drawing.Size(166, 144);
-            this.cost_chart.TabIndex = 3;
-            this.cost_chart.Text = "chart1";
+            this.cost_category.Location = new System.Drawing.Point(68, 307);
+            this.cost_category.Multiline = true;
+            this.cost_category.Name = "cost_category";
+            this.cost_category.ReadOnly = true;
+            this.cost_category.Size = new System.Drawing.Size(245, 109);
+            this.cost_category.TabIndex = 3;
+            // 
+            // income_category
+            // 
+            this.income_category.Location = new System.Drawing.Point(365, 307);
+            this.income_category.Multiline = true;
+            this.income_category.Name = "income_category";
+            this.income_category.ReadOnly = true;
+            this.income_category.Size = new System.Drawing.Size(249, 109);
+            this.income_category.TabIndex = 4;
+            // 
+            // goal
+            // 
+            this.goal.Location = new System.Drawing.Point(457, 22);
+            this.goal.Name = "goal";
+            this.goal.Size = new System.Drawing.Size(124, 40);
+            this.goal.TabIndex = 5;
+            this.goal.Text = "Цель";
+            this.goal.UseVisualStyleBackColor = true;
             // 
             // Main_screen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(678, 450);
-            this.Controls.Add(this.cost_chart);
+            this.Controls.Add(this.goal);
+            this.Controls.Add(this.income_category);
+            this.Controls.Add(this.cost_category);
             this.Controls.Add(this.action_textbox);
             this.Controls.Add(this.income);
             this.Controls.Add(this.cost);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Main_screen";
             this.Text = "Калькулятор семейного бюджета";
-            ((System.ComponentModel.ISupportInitialize)(this.cost_chart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,7 +118,9 @@
         private System.Windows.Forms.Button cost;
         private System.Windows.Forms.Button income;
         private System.Windows.Forms.TextBox action_textbox;
-        private System.Windows.Forms.DataVisualization.Charting.Chart cost_chart;
+        private System.Windows.Forms.TextBox cost_category;
+        private System.Windows.Forms.TextBox income_category;
+        private System.Windows.Forms.Button goal;
     }
 }
 
--- Main_screen.cs
+++ Main_screen.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,15 +15,78 @@ namespace budget_calculator
     {
         public Main_screen()
         {
+            Dictionary<string, int> category_cost = new Dictionary<string, int>();
+            Dictionary<string, int> category_income = new Dictionary<string, int>();
+            float count_cost = 0;
+            float count_income = 0;
             var action_list = new List<string>();
             action_list.Add("Номенал \tНаименование \tКатегория \tДата\r\n");
-            action_list.Add("-1000 \tЗаправка \tАвтомобиль \t12.04.22\r\n");
-            action_list.Add("+10000 \tЗарплата жены \tЗаработная плата \t9.04.22\r\n");
+            string filename = "D:\\C#\\budget-calculator\\budget-calculator\\Action.txt";
+            using (var sr = new StreamReader(filename))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    bool cost_operation = true;
+                    var elems = line.Split(';');
+                    if (elems[0][0] == '+')
+                        cost_operation = false;
+
+                    if (elems[1].Length > 17)
+                        elems[1] = elems[1].Substring(0, 15) + '…';
+
+                    if (cost_operation)
+                    {
+                        int val = Int32.Parse(elems[0].Substring(1, elems[0].Length-1));
+                        count_cost += val;
+                        if (category_cost.TryGetValue(elems[2], out int i))
+                            category_cost[elems[2]] += val;
+                        else
+                        {
+                            category_cost.Add(elems[2], val);
+                        }
+                    }
+                    else
+                    {
+                        int val = Int32.Parse(elems[0].Substring(1, elems[0].Length-1));
+                        count_income += val;
+                        if (category_income.TryGetValue(elems[2], out int i))
+                            category_income[elems[2]] += val;
+                        else
+                        {
+                            category_income.Add(elems[2], val);
+                        }
+                    }
+
+
+                    if (elems[0].Length > 8)
+                        elems[0] = elems[0].Substring(0, 7) + '…';
+
+                    if (elems[2].Length > 17)
+                        elems[2] = elems[2].Substring(0, 15) + '…';
+
+                    if (elems[3].Length > 8)
+                        elems[3] = elems[3].Substring(0, 7) + '…';
+
+                    action_list.Add(String.Format("{0,-3}\t{1,-17}\t{2,-17}\t{3,-10}\r\n", elems[0], elems[1], elems[2], elems[3]));
+                }
+            }
             InitializeComponent();
             foreach (var action in action_list)
             {
                 action_textbox.Text += action;
             }
+            foreach (var category in category_cost)
+            {
+                var procent = Math.Round((category.Value / count_cost), 3)*100;
+                cost_category.Text += $"{category.Key} {procent}%\r\n";
+            }
+            foreach (var category in category_income)
+            {
+                var procent = Math.Round((category.Value / count_income), 3) * 100;
+                income_category.Text += $"{category.Key} {procent}%\r\n";
+            }
+
         }
 
         private void cost_Click(object sender, EventArgs e)
@@ -31,6 +95,11 @@ namespace budget_calculator
         }
 
         private void action_textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void income_Click(object sender, EventArgs e)
         {
 
         }
